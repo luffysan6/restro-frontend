5bd972cd886d09c618d9b450f5383d0a81815302
Dhruv Rohilla <dhruvrohilla666@gmail.com>
2026-05-13 19:19:35 +0530

    Added Backend URL

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://restro-backend-wfvm.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
